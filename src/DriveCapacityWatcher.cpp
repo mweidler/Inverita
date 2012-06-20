01c1a51fb792c0cb3241f5bbcdc2ffa8f8a29778
Marc Weidler <marc.weidler@web.de>
2012-06-20 06:57:49 +0200

    Keep at least one old snapshot

--- src/DriveCapacityWatcher.cpp
+++ src/DriveCapacityWatcher.cpp
@@ -87,14 +87,18 @@ void DriveCapacityWatcher::setAutoDeleteEnabled(bool autoDelete)
     qDebug() << "DriveCapacityWatcher setAutoDelete" << autoDelete;
 }
 
+
 void DriveCapacityWatcher::update()
 {
     qreal capacity = m_capacityModel->capacity();
     qDebug() << "watch: " << m_backupRootPath << capacity;
 
-    if (capacity < 0.05 && m_historyListModel->size() > 0 && m_autoDelete) {
+    if (capacity < 0.05 && m_autoDelete) {
+        // keep at least 1 previous snapshot
+        if (m_historyListModel->size() > 1) {
         m_snapshotName = m_backupRootPath + "/" + m_historyListModel->at(0).name;
         start();
+        }
     }
 }
 
