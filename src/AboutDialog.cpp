8b61e602be6b9ed1059f77784498965122a56472
Marc Weidler <marc.weidler@web.de>
2012-09-06 17:49:22 +0200

    Fix layout bugs

--- src/AboutDialog.cpp
+++ src/AboutDialog.cpp
@@ -56,12 +56,13 @@ AboutDialog::AboutDialog(QWidget *parent) : QDialog(parent)
             "Copyright (C) 2012 Nokia Corporation and/or its subsidiary(-ies).<br><br>"
             "INVERITA makes use of EncFS (http://www.arg0.net/encfs)<br>"
             "Copyright (C) 2003-2007, Valient Gough (vgough@pobox.com)<br><br>"
-            "INVERITA uses parts of PolarSSL library (http://www.polarssl.org)<br>"
-            "FIPS-180-1 compliant SHA-1 implementation<br>"
+            "INVERITA contains FIPS-180-1 compliant SHA-1 implementation from PolarSSL library (http://www.polarssl.org) "
             "Copyright (C) 2006-2010, Brainspark B.V.<br><br>"
-            "INVERITA uses icons from Artua (http://www.artua.com)<br>"
+            "INVERITA contains icons from Artua (http://www.artua.com)<br>"
             "that are free for non-commercial use.<br><br>"
-            "INVERITA uses icons from Joker Design (http://www.joker-design.com)<br>"
+            "INVERITA contains icons from the 'Faenza' theme,<br>"
+            "created by XXX.<br><br>"
+            "INVERITA contains icons from Joker Design (http://www.joker-design.com)<br>"
             "that are licensed under 'CC Attribution-Noncommercial-No Derivate 3.0'.<br>"
         );
 
@@ -87,7 +88,7 @@ AboutDialog::AboutDialog(QWidget *parent) : QDialog(parent)
     contentLayout->setAlignment(Qt::AlignLeft);
     contentLayout->addLayout(imagesLayout);
     contentLayout->addSpacerItem(new QSpacerItem(20, 0));
-    contentLayout->addWidget(labelAboutText, 1);
+    contentLayout->addWidget(labelAboutText, 1, Qt::AlignTop);
 
     QVBoxLayout *masterLayout = new QVBoxLayout;
     masterLayout->setContentsMargins(20, 20, 20, 20);
@@ -97,8 +98,7 @@ AboutDialog::AboutDialog(QWidget *parent) : QDialog(parent)
 
     connect(buttonBox, SIGNAL(accepted()), this, SLOT(accept()));
 
-    setMinimumSize(700, 580);
-    resize(700, 580);
+    setMinimumSize(700, 500);
     setWindowTitle(tr("About INVERITA Personal Backup"));
 }
 
